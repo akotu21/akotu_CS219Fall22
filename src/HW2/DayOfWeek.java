d3dd6c0d053f96bd64ce91ccc63e1cc53e833155
akotu <augustkotula@gmail.com>
2022-09-07 23:06:36 -0400

    DayOfWeek homework

--- src/HW2/DayOfWeek.java
+++ src/HW2/DayOfWeek.java
@@ -1,3 +1,10 @@
+/*
+August Kotula
+9/6/2022
+HW2
+ */
+
+
 package HW2;
 
 import java.util.Scanner;
@@ -15,18 +22,18 @@ public class DayOfWeek {
     }
     public static void main(String [] args){
 
-    Scanner kbd = new Scanner(System.in);
+        Scanner kbd = new Scanner(System.in);
 
-    System.out.print("Enter a month (1-12): ");
-    int m = kbd.nextInt();
+        System.out.print("Enter a month (1-12): ");
+        int m = kbd.nextInt();
 
-    System.out.print("Enter a day (1-31): ");
-    int d = kbd.nextInt();
+        System.out.print("Enter a day (1-31): ");
+        int d = kbd.nextInt();
 
-    System.out.print("Enter a year (4 digits): ");
-    int y = kbd.nextInt();
+        System.out.print("Enter a year (4 digits): ");
+        int y = kbd.nextInt();
 
-    System.out.print(dayofweek(m, d, y));
+        System.out.print(dayofweek(m, d, y));
 
     }
 }
